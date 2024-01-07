a769d1511f2673dd8ab0e97951490269d52440d9
wilbill <ntabazibilly57@gmail.com>
2024-01-06 22:13:45 -0600

    Added Class Component

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react"; //import component from react
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
           <FirstComponent></FirstComponent>
           <SecondComponent></SecondComponent>
           <ThirdComponent></ThirdComponent>
+          <FourthComponent></FourthComponent>
+          <FifthComponent></FifthComponent>
         </p>
         <a
           className="App-link"
@@ -25,15 +28,28 @@ function App() {
     </div>
   );
 }
+//Functional components
 function FirstComponent() {
-  return (<div className="FirstComponent">First Component</div>); //whatever we return shd be in brackets
+  return <div className="FirstComponent">First Component</div>; //whatever we return shd be in brackets
 }
-function SecondComponent(){
-  return (<div>This is the second Component</div>);
+function SecondComponent() {
+  return <div className="SecondComponent">This is the second Component</div>;
 }
 
-function ThirdComponent(){
-  return <div>This is my third component</div>
+function ThirdComponent() {
+  return <div className="ThirdComponent">This is my third component</div>;
+}
+//class components, shd have method/function called render
+class FourthComponent extends Component {
+  render() {
+    return <div className="FourthComponent">This is my Fourth component</div>;
+  }
+}
+
+class FifthComponent extends Component{
+  render() {
+    return <div className="FifthComponent">This is the fifth Component</div>;
+  }
 }
 
 export default App;
